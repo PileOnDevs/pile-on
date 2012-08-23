a7e8e319f58c8e2f5d9d96b29d13b20abe817dc9
Matt Bailey <ambiturner0@gmail.com>
2012-08-22 19:11:30 -0700

    08-22-2012: Initial upload of the bare bones app

---
+++ src/com/game/pileon/GameMenu.java
@@ -0,0 +1,41 @@
+package com.game.pileon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class GameMenu extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_game_menu, menu);
+        return true;
+    }
+    public void about(View view) {
+    	Intent intent = new Intent(this, AboutScreen.class);
+    	startActivity(intent);
+    }
+    
+    public void help(View view) {
+    	Intent intent = new Intent(this, HelpScreen.class);
+    	startActivity(intent);
+    }
+    
+    public void launchGame(View view) {
+    	Intent intent = new Intent(this, MainGame.class);
+    	startActivity(intent);
+    }
+    
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    
+}
