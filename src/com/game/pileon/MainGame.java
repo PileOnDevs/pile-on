c2d0dcb9e2fd99ac9e8d2365905182d2f18c8093
breeze4 <ambiturner0@gmail.com>
2013-01-13 12:19:13 -0800

    Added all hands and piles to the game

--- src/com/game/pileon/MainGame.java
+++ src/com/game/pileon/MainGame.java
@@ -46,22 +46,58 @@ implements View.OnTouchListener
 	{
 		//setup PileViews - three each
 		PileView pileView0 = new PileView(this, mGameEngine.Pile0);		
-	    //sizing the PileView
 	    DragLayer.LayoutParams pileView0params = new DragLayer.LayoutParams(pileView0.getDrawable().getIntrinsicWidth(), 
 	    		pileView0.getDrawable().getIntrinsicHeight(), 120, 320);
 	    mDragLayer.addView(pileView0, pileView0params);
 	    mDragController.addDropTarget(pileView0);
+	    
+	    PileView pileView1 = new PileView(this, mGameEngine.Pile1);		
+	    DragLayer.LayoutParams pileView1params = new DragLayer.LayoutParams(pileView1.getDrawable().getIntrinsicWidth(), 
+	    		pileView1.getDrawable().getIntrinsicHeight(), 280, 320);
+	    mDragLayer.addView(pileView1, pileView1params);
+	    mDragController.addDropTarget(pileView1);
+	    
+	    PileView pileView2 = new PileView(this, mGameEngine.Pile2);		
+	    DragLayer.LayoutParams pileView2params = new DragLayer.LayoutParams(pileView2.getDrawable().getIntrinsicWidth(), 
+	    		pileView2.getDrawable().getIntrinsicHeight(), 420, 320);
+	    mDragLayer.addView(pileView2, pileView2params);
+	    mDragController.addDropTarget(pileView2);
 
 	    //setup HandViews - five each
 	    HandView handView0 = new HandView(this, mGameEngine.Hand0);
-		handView0.setOnTouchListener(this);
-	    Log.i("PO CreateDeck", "handView0 LayoutParams width: " + handView0.getDrawable().getIntrinsicWidth() +
-	    		" height: "+ handView0.getDrawable().getIntrinsicHeight());	    
+		handView0.setOnTouchListener(this);    
 	    DragLayer.LayoutParams handView0params = new DragLayer.LayoutParams(handView0.getDrawable().getIntrinsicWidth(), 
-	    		handView0.getDrawable().getIntrinsicHeight(), 120, 120);
-	    
+	    		handView0.getDrawable().getIntrinsicHeight(), 60, 120);
 	    mDragLayer.addView(handView0, handView0params);
 		handView0.setHand(mGameEngine.Hand0);
+		
+		HandView handView1 = new HandView(this, mGameEngine.Hand1);
+		handView1.setOnTouchListener(this);    
+	    DragLayer.LayoutParams handView1params = new DragLayer.LayoutParams(handView1.getDrawable().getIntrinsicWidth(), 
+	    		handView1.getDrawable().getIntrinsicHeight(), 180, 120);
+	    mDragLayer.addView(handView1, handView1params);
+	    handView1.setHand(mGameEngine.Hand1);
+		
+	    HandView handView2 = new HandView(this, mGameEngine.Hand2);
+	    handView2.setOnTouchListener(this);    
+	    DragLayer.LayoutParams handView2params = new DragLayer.LayoutParams(handView2.getDrawable().getIntrinsicWidth(), 
+	    		handView2.getDrawable().getIntrinsicHeight(), 300, 120);
+	    mDragLayer.addView(handView2, handView2params);
+	    handView2.setHand(mGameEngine.Hand2);
+	    
+	    HandView handView3 = new HandView(this, mGameEngine.Hand3);
+	    handView3.setOnTouchListener(this);    
+	    DragLayer.LayoutParams handView3params = new DragLayer.LayoutParams(handView3.getDrawable().getIntrinsicWidth(), 
+	    		handView3.getDrawable().getIntrinsicHeight(), 420, 120);
+	    mDragLayer.addView(handView3, handView3params);
+	    handView3.setHand(mGameEngine.Hand3);
+	    
+	    HandView handView4 = new HandView(this, mGameEngine.Hand4);
+	    handView4.setOnTouchListener(this);    
+	    DragLayer.LayoutParams handView4params = new DragLayer.LayoutParams(handView4.getDrawable().getIntrinsicWidth(), 
+	    		handView4.getDrawable().getIntrinsicHeight(), 540, 120);
+	    mDragLayer.addView(handView4, handView4params);
+	    handView4.setHand(mGameEngine.Hand4);
 	}
 	
 	public int getDrawable(Context context, String name)
